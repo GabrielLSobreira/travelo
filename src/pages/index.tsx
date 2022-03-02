4f49303b4fe2e4a6984b57b18bbab472de6cac0e
GabrielLSobreira <gabriellaranjasobreira@gmail.com>
2022-03-02 07:45:46 -0300

    feat(app): initial setup and add libs

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Travelo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
